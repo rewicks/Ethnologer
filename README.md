d222f69321c221b4a39a40b9a4682e017762d1a7
rachel wicks <rwicks@login2.clsp.jhu.edu>
2019-10-30 18:04:55 -0400

    version 1.0

---
+++ README.md
@@ -0,0 +1,45 @@
+# Build:
+
+You can build a new ethnologue::
+
+```
+import ethnologer
+ethnologer.save_model(model_path, typological_rules_path, ethnologue_paths)
+
+# for example:
+ethnologer.save_model('ETHNOLOGUE', 'min-parse.txt', glob.glob('ethnologue/*'))
+
+```
+
+# Load
+
+You can import ethnologer, and then load a saved model:
+```
+import ethnologer
+
+ethnologue = ethnologer.load_model(model_path)
+```
+
+Then you can get a language:
+```
+ethnologue.languages['eng']
+```
+From the language, you can get the parent family, or the typological features:
+```
+eng = ethnologue.languages['eng']
+eng.typological_features # {'SVO', 'CAUSATIVES', ... }
+eng.parent_family # 'ENGLISH' <-- LanguageFamily Class
+```
+
+You can also find a language family (provided it is the most senior language family in its family:
+
+```
+ethnologue.families['Indo-European'] # correct!
+ethnologue.families['Germanic'] # throws error
+```
+You can find a daughter family, by accessing the daughter family list:
+```
+indo_eur = ethnologue.families['Indo-European']
+indo_eur.daughter_families # ['Albanian', 'Germanic', ....] <--each is a LanguageFamily
+indo_eur.common_typological_features # {'SVO'}
+```
--- ethnologer.py
+++ ethnologer.py
@@ -3,10 +3,33 @@ import pickle as pkl
 import os
 import argparse
 
+
+'''
+Ethnologue class becomes an easily searchable class of languages and families present in ethnologue
+    -Can only index by the most parent family
+    -Index language by language code
+'''
+
 class Ethnologue():
-    def __init__(self):
+    def __init__(self, classifier, ethnologue_files):
         self.families = {}
         self.languages = {}
+        self.classifer = classifier
+        for fi in ethnologue_files:
+            name = fi.split('/')[-1][:-5]
+            content = open(fi, 'r').read()
+            typ = self.get_typological_info(content)
+            fams = self.get_family_info(content)
+            self.build_language(name, fams)
+        
+            if typ != -1:
+                features = classifier.get_features(typ)
+                for f in features:
+                    self.languages[name].add_typological_feature(f)
+
+        for family in self.families:
+            if self.families[family].common_typological_features is None:
+                self.families[family].set_common_typological_features()
 
     def add_family(self, family):
         if family.name not in self.families:
@@ -17,6 +40,80 @@ class Ethnologue():
     def add_language(self, language):
         self.languages[language.name] = language
 
+    def build_families(self, fam):
+        if fam[0] not in self.families:
+            root  = LanguageFamily(fam[0])
+        else:
+            root = self.families[fam[0]]
+        temp_fam = root
+        for index, family in enumerate(fam[1:], 1):
+            if family not in [daughter.name for daughter in temp_fam.daughter_families]:
+                temp_fam = LanguageFamily(family, temp_fam)
+            else:
+                for d in temp_fam.daughter_families:
+                    if family == d.name:
+                        temp_fam = d
+                        break
+        self.add_family(root)
+        return temp_fam
+
+    def build_language(self, lang, fam):
+        if fam != -1:
+            parent = self.build_families(fam)
+            self.add_language(Language(lang, parent))
+            parent.add_member(self.languages[lang])
+        else:
+            parent = LanguageFamily('Unclassified')
+            self.add_family(parent)
+            self.add_language(Language(lang, parent))
+            parent.add_member(self.languages[lang])
+
+    def get_typological_info(self, content):
+        typ = self.get_label(content, 'Typology')
+        if typ is None:
+            return -1
+        typ = typ.split(';')
+        for t in range(len(typ)):
+            typ[t] = typ[t].replace('Typology', '')
+            typ[t] = typ[t].replace('.', '')
+            typ[t] = typ[t].strip()
+        return typ
+    
+    def get_family_info(self, content):
+        fam = self.get_label(content, 'Classification')
+        if fam is None:
+            return -1
+        fam = fam.replace('Classification', '')
+        fam = fam.split(',')
+        for f in range(len(fam)):
+            fam[f] = fam[f].strip()
+        return fam
+
+    # removes tags from html
+    def remove_tags(self, content):
+        PAUSE = False
+        retVal = ''
+        for c in content:
+            if c == '<':
+                PAUSE = True
+            elif c == '>':
+                PAUSE = False
+            if not PAUSE and c != '>':
+                retVal += c
+        return retVal
+
+    # parses ethnologue and gets the field (only typology or classification currently)
+    def get_label(self, content, label):
+        content = content.split('<div class="field-label">')
+        for c in content:
+            if label in c:
+                return self.remove_tags(c)
+
+'''
+LanguageFamily class contains the references to find the members (languages who have this instance as a direct parent) and daughter families (families who have this as a direct parent)
+    -common_typological_features is the intersection of all non-zero typological sets of members AND daughter families typological features
+
+'''
 class LanguageFamily():
     def __init__(self, name, parent_family=None):
         self.name = name
@@ -33,30 +130,57 @@ class LanguageFamily():
     def add_daughter(self, language_family):
         self.daughter_families.append(language_family)
 
+    '''
+        This makes the following assumptions:
+            -if there are 0 typological features, it assumes ethnologue had no entry; unclassified
+            -if there are any typological features, it assumes this is a complete list
+            -if every member of a family (and all members of daughter families) share a feature, it is a universal/common/shared feature
+            -if a feature is universal to a family, all [typologically] unclassified languages should have this feature.
+    '''
     def set_common_typological_features(self):
-        if len(self.members) == 0 and len(self.daughter_families) == 0:
-            print(self.name)
-        else:
-            if len(self.members) != 0:
-                common = self.members[0].typological_features
-                for l in self.members:
-                    if l.typological_features is None:
-                        l.set_common_typological_features()
-                    common.intersection(l.typological_features)
-            if len(self.daughter_families) != 0:
-                if common is not None:
-                    if self.daughter_families[0].common_typological_featues is not None:
-                        self.daughter_families[0].set_common_typological_features()
-                    common = self.daughter_families[0].common_typological_features
-                for d in self.daughter_families:
-                    if d.common_typological_features is not None:
+        common = set()
+        
+        # if this family has member languages; intersect their features
+        if len(self.members) != 0:
+            for l in self.members:
+                if len(common) == 0:
+                    # only intersect non-zero sets
+                    # if they are zero, it's likely because there was not a typology section in ethnologue
+                    if len(l.typological_features) > 0:
+                        common = l.typological_features
+                else:
+                    if len(l.typological_features) > 0:
+                        common = common.intersection(l.typological_features)
+        # if this family has daughter languages, intersect their common features
+        if len(self.daughter_families) != 0:
+            if len(common) == 0:
+                # only intersect non-zero sets
+                # if they are 0, it's likely because their members were unclassified
+                # the above assumption may need to be revised since it could be that the daughter family has no universal features
+                if self.daughter_families[0].common_typological_features is None:
+                    self.daughter_families[0].set_common_typological_features()
+                common = self.daughter_families[0].common_typological_features
+            for d in self.daughter_families:
+                if len(common) == 0:
+                    if d.common_typological_features is None:
                         d.set_common_typological_features()
-                    common = common.intersection(d.common_typological_features)
-            
-            self.common_typological_features = common
+                    if len(d.common_typological_features) > 0:
+                        common = d.common_typological_features
+                else:
+                    if d.common_typological_features is None:
+                        d.set_common_typological_features()
+                    if len(d.common_typological_features) > 0:
+                        common = common.intersection(d.common_typological_features)
+        self.common_typological_features = common
+
+    def __str__(self):
+        return self.name
 
 
-# typological features should be a python set
+'''
+    Language has a parent family and typological features extracted from Ethnologue
+
+'''
 class Language():
     def __init__(self, name, parent_family=None):
         self.name = name
@@ -66,13 +190,19 @@ class Language():
     def add_typological_feature(self, feature):
         self.typological_features.add(feature)
 
+
+'''
+    TypologicalRules takes in a pre-defined rule-set that was written based on the most common typological descriptions found on Ethnologue
+        -determines if a given description satisfies any of these rules
+        -currently does not have mutually-exclusive categories
+'''
 class TypologicalRules():
     def __init__(self, file_path):
         with open(file_path, 'r') as f:
             self.rules = json.loads(f.read())['rules']
 
     def satisfies(self, tag, description):
-        
+        # replaces A for S to be uniform in description
         good = ['svo', 'sov', 'osv', 'ovs', 'vso', 'vos']
         bad = ['avo', 'aov', 'oav', 'ova', 'vao', 'voa']
         for g in range(len(good)):
@@ -102,112 +232,24 @@ class TypologicalRules():
                         retVal.append(r)
         return retVal
 
-def remove_tags(content):
-    PAUSE = False
-    retVal = ''
-    for c in content:
-        if c == '<':
-            PAUSE = True
-        elif c == '>':
-            PAUSE = False
-        if not PAUSE and c != '>':
-            retVal += c
-    return retVal
 
-def get_label(content, label):
-    content = content.split('<div class="field-label">')
-    for c in content:
-        if label in c:
-            return remove_tags(c)
+def load_model(model_path):
+    return pkl.load(open(model_path, 'rb'))
 
-def add_language_to_family(language, family):
-    families_file = open('families.pkl', 'rb')
-    family_dict = pkl.load(families_file)
-    if language not in family_dict[family]:
-        family_dict[family].append(language)
-    families_file.close()
-    families_file = open('families.pkl', 'wb')
-    pkl.dump(family_dtc, families_file)
-
-def get_typological_info(content):
-    typ = get_label(content, 'Typology')
-    if typ is None:
-        return -1
-    typ = typ.split(';')
-    for t in range(len(typ)):
-        typ[t] = typ[t].replace('Typology', '')
-        typ[t] = typ[t].replace('.', '')
-        typ[t] = typ[t].strip()
-    return typ
-    
-def get_family_info(content):
-    fam = get_label(content, 'Classification')
-    if fam is None:
-        return -1
-    fam = fam.replace('Classification', '')
-    fam = fam.split(',')
-    for f in range(len(fam)):
-        fam[f] = fam[f].strip()
-    return fam
-
-def build_families(fam, ethno):
-    root  = LanguageFamily(fam[0])
-    temp_fam = root
-    for index, family in enumerate(fam[1:], 1):
-        temp_fam = LanguageFamily(family, temp_fam)
-    ethno.add_family(root)
-    return temp_fam
-
-def build_language(lang, fam, ethno):
-    if fam != -1:
-        parent = build_families(fam, ethno)
-        ethno.add_language(Language(lang, parent))
-        parent.add_member(ethno.languages[lang])
-          
-    else:
-        parent = LanguageFamily('Unclassified')
-        ethno.add_family(parent)
-        ethno.add_language(Language(lang, parent))
-        parent.add_member(ethno.languages[lang])
-
+def save_model(model_path, typological_dict, ethnologue_paths):
+    classifier = TypologicalRules(typological_dict)
+    ethno = Ethnologue(classifier, ethnologue_paths)
+    pkl.dump(ethno, open(model_path, 'wb'))
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
     parser.add_argument('typological_dict')
+    parser.add_argument('--output', default='ethnologue.pkl')
     parser.add_argument('ethnologue_paths', nargs='*')
 
     args = parser.parse_args()
-    
+    save_model(args.output, args.typological_dict, args.ethnologue_paths)
     classifier = TypologicalRules(args.typological_dict)
-    ethno = Ethnologue()
-
-    for fi in args.ethnologue_paths:
-        name = fi.split('/')[-1][:-5]
-        content = open(fi, 'r').read()
-        typ = get_typological_info(content)
-        fams = get_family_info(content)
-        build_language(name, fams, ethno)
-        
-        if typ != -1:
-            features = classifier.get_features(typ)
-            for f in features:
-                ethno.languages[name].add_typological_feature(f)
-
-    for family in ethno.families:
-        if ethno.families[family].common_typological_features is None:
-            ethno.families[family].set_common_typological_features()
-    ''' 
-    for lang in ethno.languages:
-        if ethno.languages[lang].parent_family is not None:
-            ethno.languages[lang].parent_family.set_common_typological_features()
-    STOP = False
-    while not STOP:
-        STOP = True
-        for family in ethno.families:
-            if ethno.families[family].common_typological_features is not None:
-                if ethno.families[family].parent_family is not None:
-                    ethno.families[family].parent_family.set_common_typological_features()
-            else:
-                STOP = False
-    '''
-    pkl.dump(ethno, open('ethno.pkl', 'wb'))
+    ethno = Ethnologue(classifier, args.ethnologue_paths)
+    
+    pkl.dump(ethno, open(args.output, 'wb'))
