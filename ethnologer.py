59b05d71f9d1c1f043456ca1226400e830b10afa
rachel wicks <rwicks@login.clsp.jhu.edu>
2019-11-22 02:37:11 -0500

    final version

--- ethnologer.py
+++ ethnologer.py
@@ -11,21 +11,34 @@ Ethnologue class becomes an easily searchable class of languages and families pr
 '''
 
 class Ethnologue():
-    def __init__(self, classifier, ethnologue_files):
+    def __init__(self, classifier, ethnologue_files, merge=False, merge_path=None):
         self.families = {}
         self.languages = {}
-        self.classifer = classifier
+        self.classifier = classifier
         for fi in tqdm.tqdm(ethnologue_files):
             name = fi.split('/')[-1][:-5]
             content = open(fi, 'r').read()
             typ = self.get_typological_info(content)
             fams = self.get_family_info(content)
             self.build_language(name, fams)
-        
+   
             if typ != -1:
                 features = classifier.get_features(typ)
                 for f in features:
                     self.languages[name].add_typological_feature(f)
+
+        if merge:
+            with open(merge_path) as merge:
+                for line in merge:
+                    try:
+                        feature, languages = line.strip().split(':')
+                        for lang in languages.strip().split(';'):
+                            if lang in self.languages:
+                                self.languages[lang].add_typological_feature(feature.strip())
+                            else:
+                                print(f'{lang} not in ethnologue')
+                    except:
+                        print(line)
 
         for family in self.families:
             if self.families[family].common_typological_features is None:
@@ -117,10 +130,15 @@ class Ethnologue():
 
     def reconstruct(self):
         for l in self.languages:
-            if self.languages[l].parent_family.name != "Unclassified":
-                self.languages[l].reconstructed_typological_features = self.languages[l].parent_family.common_typological_features.copy()
+            if len(self.languages[l].typological_features) == 0: 
+                if self.languages[l].parent_family.name != "Unclassified":
+                    self.languages[l].reconstructed_typological_features = self.languages[l].parent_family.common_typological_features.copy()
+                else:
+                    self.languages[l].reconstructed_typological_features = set()
             else:
                 self.languages[l].reconstructed_typological_features = set()
+
+
 '''
 LanguageFamily class contains the references to find the members (languages who have this instance as a direct parent) and daughter families (families who have this as a direct parent)
     -common_typological_features is the intersection of all non-zero typological sets of members AND daughter families typological features
@@ -213,15 +231,14 @@ class TypologicalRules():
         with open(file_path, 'r') as f:
             self.rules = json.loads(f.read())['rules']
 
-    def satisfies(self, tag, description):
+    def satisfies(self, cat, tag, description):
         # replaces A for S to be uniform in description
         good = ['svo', 'sov', 'osv', 'ovs', 'vso', 'vos']
         bad = ['avo', 'aov', 'oav', 'ova', 'vao', 'voa']
         for g in range(len(good)):
             description = description.replace(bad[g], good[g])
-
-        if "contains" in self.rules[tag]:
-            for c in self.rules[tag]["contains"]:
+        if "contains" in self.rules[cat][tag]:
+            for c in self.rules[cat][tag]["contains"]:
                 CONT_SAT = True
                 for term in c:
                     if term not in description:
@@ -229,29 +246,35 @@ class TypologicalRules():
                 if CONT_SAT:
                     break
         NOT_CONT_SAT = True
-        if "does not contain" in self.rules[tag]:
-            for c in self.rules[tag]["does not contain"]:
+        if "does not contain" in self.rules[cat][tag]:
+            for c in self.rules[cat][tag]["does not contain"]:
                 if c in description:
                     NOT_CONT_SAT = False
         return CONT_SAT and NOT_CONT_SAT
 
     def get_features(self, descriptions):
         retVal = []
-        for d in descriptions:
-            for r in self.rules:
-                if r not in retVal:
-                    if self.satisfies(r, d.lower()):
-                        retVal.append(r)
+        for cat in self.rules:
+            temp = []
+            for d in descriptions:
+                for r in self.rules[cat]:
+                    if r not in temp:
+                        if self.satisfies(cat, r, d.lower()):
+                            temp.append(r)
+            if len(temp) > 0:
+                retVal.append('_AND_'.join(sorted(temp)))
         return retVal
 
 
 def load_model(model_path):
     return pkl.load(open(model_path, 'rb'))
 
-def save_model(model_path, typological_dict, ethnologue_paths):
+def save_model(model_path, typological_dict, ethnologue_paths, merge=False, merge_path=None):
     classifier = TypologicalRules(typological_dict)
-    ethno = Ethnologue(classifier, ethnologue_paths)
+    ethno = Ethnologue(classifier, ethnologue_paths, merge=merge, merge_path=merge_path)
     pkl.dump(ethno, open(model_path, 'wb'))
+
+
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
@@ -261,7 +284,4 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     save_model(args.output, args.typological_dict, args.ethnologue_paths)
-    classifier = TypologicalRules(args.typological_dict)
-    ethno = Ethnologue(classifier, args.ethnologue_paths)
     
-    pkl.dump(ethno, open(args.output, 'wb'))
